870291e8c2e82efa3f62ecb018176777d65da5db
Astiti Hanum A <60764341+astitihanum@users.noreply.github.com>
2021-10-25 22:06:15 +0700

    Add files via upload

---
+++ application/views/profil.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">About</h1>
+                    
+                    <!-- DataTales Example -->
+                    <div class="row">
+        <div class="col-3">
+            <img src="<?= base_url('assets/img/bs3.jpg') ?>" class="img-fluid rounded img-cover-adjust" alt="">
+        </div>
+        <div class="col">
+            <h5>Aplikasi ini dibuat oleh : </h5>
+            <p class="m-0">Nama : Astiti Hanum Anggardini</p>
+            <p class="m-0">NIM  : 1831710083</p>
+            <p class="m-0">Tanggal : 25 Oktober 2021</p>
+        </div>
+
+    </div>
+</div>
